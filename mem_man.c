76bda72e3780ce8bcba5f117cd3b951ab1ca360e
Justin <jed833@gmail.com>
2014-03-20 11:25:45 -0700

    dded mem_man stuff

--- mem_man.c
+++ mem_man.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
-#include "headers/page.h"
+#include "headers/mem_man.h"
 #include "headers/types.h"
+#include "headers/proc.h"
+#include "headers/util.h"
+
+
+#define PAGE_COUNT          60536
+#define PAGE_MASK           ( PAGE_COUNT - 1 )
+#define VAS_VEC_SIZE        ( 1 << 6 )
+#define VAS_VEC_SIZE_MASK   ( VAS_VEC_SIZE - 1 )
+
 
 
